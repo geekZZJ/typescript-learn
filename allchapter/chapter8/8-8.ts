ff85fb280c1588013b6b8b19e5a37881ac8299b5
zzj <2461927976@qq.com>
2023-08-17 00:50:44 +0800

    范型函数重载

---
+++ allchapter/chapter8/8-8.ts
@@ -0,0 +1,28 @@
+import { quickSort } from "./8-4";
+import { isChinese, sortChinese } from "./8-5";
+const chineseArr = ["武汉", "石家庄", "郑州", "太原", "济南", "沈阳", "大连"];
+
+function strSelfSort(str: string) {
+  const strArr = str.split("");
+  const strSortArr = quickSort(strArr);
+  return strSortArr.join("");
+}
+
+function sort<T>(data: T): T;
+function sort<T>(data: T) {
+  if (Array.isArray(data)) {
+    if (isChinese(data)) {
+      return sortChinese(data);
+    } else {
+      return quickSort(data);
+    }
+  } else if (typeof data === "string") {
+    return strSelfSort(data);
+  }
+  return undefined;
+}
+
+sort(chineseArr);
+sort("edsjgv");
+
+export {};
